f2beb74b5f478c69359d8520164a097bd3838d8d
wbr8 <w.bronakowski08@gmail.com>
2025-11-10 16:02:39 +0000

    Added and set nushell + a little bit of cleanup

--- hosts/T14s/default.nix
+++ hosts/T14s/default.nix
@@ -17,8 +17,6 @@
   boot.kernelPackages = pkgs.linuxPackages_latest;
 
   networking.hostName = "T14s"; # Define your hostname.
-  # Pick only one of the below networking options.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
   networking.networkmanager.enable = true;  # Easiest to use and most distros use this by default.
 
   # Set your time zone.
@@ -36,13 +34,15 @@
     useXkbConfig = false; # use xkb.options in tty.
   };
 
-  # services.displayManager.cosmic-greeter.enable = true;
-  # services.desktopManager.cosmic.enable = true;
-  
+  environment.shells = [
+    pkgs.nushell
+  ];
 
-  # Configure keymap in X11
-  # services.xserver.xkb.layout = "us";
-  # services.xserver.xkb.options = "eurosign:e,caps:escape";
+  programs.bash.interactiveShellInit = ''
+    if ! [ "$TERM" = "dumb" ]; then
+      exec nu
+    fi
+  '';
 
   # Enable CUPS to print documents.
   # services.printing.enable = true;
@@ -50,7 +50,6 @@
   # Enable touchpad support (enabled default in most desktopManager).
   services.libinput.enable = true;
 
-  # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.wojtek = {
     isNormalUser = true;
     extraGroups = [ "wheel" "input" "output" "video" "audio" ]; # Enable ‘sudo’ for the user.
@@ -60,8 +59,7 @@
   };
 
   programs.nix-ld.enable = true;
-  # List packages installed in system profile.
-  # You can use https://search.nixos.org/ to find more packages (and options).
+
   environment.systemPackages = with pkgs; [
     vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     wget

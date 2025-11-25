d3d00f8df94f7ce5e462207c3694592e0445c1d5
wbr8 <w.bronakowski08@gmail.com>
2025-11-25 10:29:55 +0000

    Set Zen as default browser for various things

--- home.nix
+++ home.nix
@@ -12,6 +12,36 @@
 
   programs.zen-browser.enable = true;
 
+  xdg.mimeApps = let
+    value = let
+      zen-browser = inputs.zen-browser.packages."x86_64-linux".beta; # or twilight
+    in
+      zen-browser.meta.desktopFileName;
+
+    associations = builtins.listToAttrs (map (name: {
+        inherit name value;
+      }) [
+        "application/x-extension-shtml"
+        "application/x-extension-xhtml"
+        "application/x-extension-html"
+        "application/x-extension-xht"
+        "application/x-extension-htm"
+        "x-scheme-handler/unknown"
+        "x-scheme-handler/mailto"
+        "x-scheme-handler/chrome"
+        "x-scheme-handler/about"
+        "x-scheme-handler/https"
+        "x-scheme-handler/http"
+        "application/xhtml+xml"
+        "application/json"
+        "text/plain"
+        "text/html"
+      ]);
+  in {
+    associations.added = associations;
+    defaultApplications = associations;
+  };
+  
   programs.helix.enable = true;
   programs.helix.settings = {
     editor = {

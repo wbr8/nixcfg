156628ccc334135e3ae7d6e4bc7ca2aa9faf0982
wbr8 <w.bronakowski08@gmail.com>
2026-02-11 10:45:15 +0000

    Enable Steam

--- hosts/T14s/default.nix
+++ hosts/T14s/default.nix
@@ -76,6 +76,8 @@
     ripgrep
   ];
 
+  programs.steam.enable = true;
+
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   # programs.mtr.enable = true;

1a125bdc9c9ea661587faf528387909e229303c6
wbr8 <w.bronakowski08@gmail.com>
2026-07-23 23:41:59 +0100

    flake: create a base for the dendritic pattern

--- flake.nix
+++ flake.nix
@@ -1,11 +1,56 @@
 {
   description = "nixos configuration";
 
-  outputs = inputs: inputs.flake-parts.lib.mkFlake { inherit inputs; } (inputs.import-tree ./modules);
+  nixConfig = {
+    extra-substituters = [
+    ];
 
-  inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    flake-parts.url = "github:hercules-ci/flake-parts";
-    import-tree.url  = "github:denful/import-tree";
+    extra-trusted-public-keys = [
+    ];
+
+    experimental-features = [
+      "flakes"
+      "nix-command"
+      "pipe-operators"
+    ];
+
+    warn-dirty = false;
+  };
+
+  outputs =
+    inputs:
+    (import "${inputs.flake-parts}/lib.nix" { lib = import ./lib inputs.nixpkgs.lib; }).mkFlake
+      { inherit inputs; }
+      (
+        { lib, ... }:
+        let
+          inherit (lib.filesystem) listFilesRecursive;
+          inherit (lib.lists) filter;
+          inherit (lib.strings) hasSuffix;
+        in
+        {
+          systems = [
+            "aarch64-darwin"
+            "aarch64-linux"
+            "x86_64-linux"
+          ];
+
+          imports = filter (hasSuffix ".mod.nix") (listFilesRecursive ./.);
+        }
+      );
+
+  inputs.nixpkgs = {
+    url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  inputs.flake-parts = {
+    url = "github:hercules-ci/flake-parts";
+    inputs.nixpkgs-lib.follows = "nixpkgs";
+  };
+
+  inputs.helium = {
+    url = "github:amaanq/helium-flake";
+    inputs.nixpkgs.follows = "nixpkgs";
   };
 }
+
